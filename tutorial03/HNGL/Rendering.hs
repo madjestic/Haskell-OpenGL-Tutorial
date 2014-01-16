b27c7c0b42f41a8497407a9f70588dcdbdcccce3
madjestic <madjestic13@gmail.com>
2014-01-16 01:27:01 +0100

    adding tutorial03 files

---
+++ tutorial03/HNGL/Rendering.hs
@@ -0,0 +1,78 @@
+module HNGL.Rendering where
+
+import Graphics.Rendering.OpenGL as GL
+import Graphics.UI.GLFW as GLFW
+import Control.Monad
+import Foreign.Marshal.Array
+import Foreign.Ptr
+import Foreign.Storable
+import HNGL.LoadShaders
+import HNGL.Data
+
+
+data Descriptor = Descriptor VertexArrayObject ArrayIndex NumArrayIndices
+
+
+bufferOffset :: Integral a => a -> Ptr b
+bufferOffset = plusPtr nullPtr . fromIntegral
+
+
+initResources :: [Vertex2 Float] -> IO Descriptor
+initResources vs = do
+    triangles <- genObjectName
+    bindVertexArrayObject $= Just triangles
+
+
+    let vertices = vs
+        numVertices = length vertices
+
+    arrayBuffer <- genObjectName
+    bindBuffer ArrayBuffer $= Just arrayBuffer
+    withArray vertices $ \ptr -> do
+        let size = fromIntegral (numVertices * sizeOf (head vertices))
+        bufferData ArrayBuffer $= (size, ptr, StaticDraw)
+
+    program <- loadShaders [
+        ShaderInfo VertexShader (FileSource "Shaders/triangles.vert"),
+        ShaderInfo FragmentShader (FileSource "Shaders/triangles.frac")]
+    currentProgram $= Just program
+
+    let firstIndex = 0
+        vPosition = AttribLocation 0
+    vertexAttribPointer vPosition $=
+        (ToFloat, VertexArrayDescriptor 2 Float 0 (bufferOffset firstIndex))
+    vertexAttribArray vPosition $= Enabled
+
+    return $ Descriptor triangles firstIndex (fromIntegral numVertices)
+
+
+resizeWindow :: Size -> IO ()
+resizeWindow size@(GL.Size w h) =
+    do
+        GL.viewport   $= (GL.Position 0 0, size)
+        GL.matrixMode $= GL.Projection
+        GL.loadIdentity
+        GL.ortho2D 0 (realToFrac w) (realToFrac h) 0
+
+
+draw :: Drawable -> IO ()
+draw xs = do
+    GLFW.initialize
+    GLFW.openWindow (GL.Size 640 480) [] GLFW.Window
+    GLFW.windowTitle $= "GLFW Demo"
+    GLFW.windowSizeCallback $= resizeWindow
+    descriptor <- initResources xs
+    onDisplay descriptor
+    GLFW.closeWindow
+    GLFW.terminate
+
+
+onDisplay :: Descriptor -> IO ()
+onDisplay descriptor@(Descriptor triangles firstIndex numVertices) = do
+    GL.clearColor $= Color4 1 0 0 1
+    GL.clear [ColorBuffer]
+    bindVertexArrayObject $= Just triangles
+    drawArrays Triangles firstIndex numVertices
+    GLFW.swapBuffers
+    p <- GLFW.getKey GLFW.ESC
+    unless (p == GLFW.Press) $ onDisplay descriptor
---
+++ tutorial03/Main.hs
@@ -0,0 +1,11 @@
+module Main where
+
+import HNGL.Data
+import HNGL.Rendering
+
+display :: [Instanceable] -> IO ()
+display xs = draw $ concat $ map toDrawable xs
+
+main :: IO ()
+main = do
+    display [(Square (-0.5, -0.5) 1.0),(Circle (0.5, 0.5) 0.5 30)]
