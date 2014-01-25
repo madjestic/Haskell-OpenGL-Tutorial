8b04de9ec375e628cb46e53e1a21f52de8f64e3f
madjestic <madjestic13@gmail.com>
2014-01-25 22:49:47 +0100

    porting to GLFW-b

--- tutorial01/Main.hs
+++ tutorial01/Main.hs
@@ -1,36 +1,53 @@
 import Graphics.Rendering.OpenGL as GL
 import Graphics.UI.GLFW as GLFW
 import Control.Monad
+import System.Exit ( exitWith, ExitCode(..) )
 
 
-resizeWindow :: Size -> IO ()
-resizeWindow =
-  \ size@(GL.Size w h) ->
+resizeWindow :: GLFW.WindowSizeCallback
+resizeWindow win w h =
     do
-      GL.viewport   $= (GL.Position 0 0, size)
+      GL.viewport   $= (GL.Position 0 0, GL.Size (fromIntegral w) (fromIntegral h))
       GL.matrixMode $= GL.Projection
       GL.loadIdentity
       GL.ortho2D 0 (realToFrac w) (realToFrac h) 0
 
-  
+
+keyPressed :: GLFW.KeyCallback 
+keyPressed win GLFW.Key'Escape _ GLFW.KeyState'Pressed _ = shutdown win
+keyPressed _   _               _ _                     _ = return ()
+
+
+shutdown :: GLFW.WindowCloseCallback
+shutdown win = do
+  GLFW.destroyWindow win
+  GLFW.terminate
+  _ <- exitWith ExitSuccess
+  return ()
+
+
 main :: IO ()
 main = do
-  GLFW.initialize
-  GLFW.openWindow (GL.Size 640 480) [] GLFW.Window
-  GLFW.windowTitle $= "GLFW Demo"
-  GLFW.windowSizeCallback $= resizeWindow
-  onDisplay
-  GLFW.closeWindow
-  GLFW.terminate
+   GLFW.init
+   GLFW.defaultWindowHints
+   Just win <- GLFW.createWindow 640 480 "GLFW Demo" Nothing Nothing
+   GLFW.makeContextCurrent (Just win)
+   GLFW.setWindowSizeCallback win (Just resizeWindow)
+   GLFW.setKeyCallback win (Just keyPressed)
+   GLFW.setWindowCloseCallback win (Just shutdown)
+   onDisplay win
+   GLFW.destroyWindow win
+   GLFW.terminate
 
 
-onDisplay :: IO ()
-onDisplay = do
+onDisplay :: Window -> IO ()
+onDisplay win = do
   GL.clearColor $= Color4 1 0 0 1
   GL.clear [ColorBuffer]
-  GLFW.swapBuffers
-
-  p <- GLFW.getKey GLFW.ESC
-  unless (p == GLFW.Press) $ 
-    onDisplay
+  GLFW.swapBuffers win
+  
+  forever $ do
+     GLFW.pollEvents
+     onDisplay win
+     
 
