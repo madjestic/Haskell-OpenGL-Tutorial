2fe9d817877f06c69054cbd1867a7dacfef0d6c7
madjestic <madjestic13@gmail.com>
2014-05-28 01:07:46 +0200

    addding events loop, based on reactive-banana-glfw

--- tutorial08/Main.hs
+++ tutorial08/Main.hs
@@ -1,23 +1,48 @@
+{-# LANGUAGE Rank2Types #-}
+
 module Main where
 
 import NGL.Shape
 import NGL.Rendering
-import TinyMath.TinyMath
-import Data.Numbers.Primes
 
-scale :: Float
-scale = 0.08
+import Control.Applicative
+import System.Exit       ( exitSuccess )
+import Graphics.UI.GLFW (Window)
+import Reactive.Banana as R
+import Reactive.Banana.Frameworks
+import Reactive.Banana.GLFW
 
-fromSpiral :: (Float, Float, Int) -> Drawable
-fromSpiral (x,y,k) 
-                | isPrime  k = toDrawable Red  $ Square (x, y) scale
-                | otherwise  = toDrawable Blue $ Square (x, y) scale
 
 main :: IO ()
-main = do   
-     let scaledSpiral = map (\(a,b,c) -> (scale*a,scale*b,c)) $ walkSpiral (0,0) (round $ 1/scale)
-     let primeSpiral = map fromSpiral scaledSpiral
+main = do
+     let drawables = [
+                      toDrawable White $ Circle (0.0, 0.0) 0.5 100       
+                     ]
 
-     window <- createWindow "NGL is Not GLoss" (900,900)
-     drawIn Default window primeSpiral
+     window <- createWindow "NGL is Not GLoss" (512,512)     
+     withEventsIn window     
+     drawIn Default window drawables
      closeWindow window
+
+
+withEventsIn :: Window -> IO ()
+withEventsIn window = do
+     h <- windowHandler window
+     network <- compile $ do
+         keyE <- keyEvent h
+         reactimate $ exit window <$ filterE (match Key'Escape) keyE
+         reactimate $ foo <$ filterE (match Key'H) keyE
+         reactimate $ print <$> keyE
+         c <- cursor h TopLeft
+         reactimate $ putStrLn . ("Cursor: " ++) . show <$> cursorMove c
+     actuate network
+
+
+foo :: IO ()
+foo = putStrLn "hello!"
+
+exit :: Window -> IO ()
+exit win = 
+     do
+      shutdown win
+      exitSuccess
