33f8c06814eb8385ea91d71e56f657d99515177e
madjestic <madjestic13@gmail.com>
2014-05-29 15:25:01 +0200

    shtuff

--- tutorial08/Main.hs
+++ tutorial08/Main.hs
@@ -6,7 +6,6 @@ import NGL.Shape
 import NGL.Rendering
 
 import Control.Applicative
-import System.Exit       ( exitSuccess )
 import Graphics.UI.GLFW (Window)
 import Reactive.Banana as R
 import Reactive.Banana.Frameworks
@@ -16,33 +15,42 @@ import Reactive.Banana.GLFW
 main :: IO ()
 main = do
      let drawables = [
+                 -- | toDrawable Color $ Shape  Position Radius Divisions
                       toDrawable White $ Circle (0.0, 0.0) 0.5 100       
                      ]
 
      window <- createWindow "NGL is Not GLoss" (512,512)     
-     withEventsIn window     
-     drawIn Default window drawables
+     withEventsIn window drawables
+--     drawIn Default window drawables
      closeWindow window
 
 
-withEventsIn :: Window -> IO ()
-withEventsIn window = do
+parm :: Integer
+parm = 2
+
+foo :: IO Int
+foo = return (1::Int)
+
+
+withEventsIn :: Window -> [Drawable] -> IO ()
+withEventsIn window ds = do
      h <- windowHandler window
      network <- compile $ do
          keyE <- keyEvent h
          reactimate $ exit window <$ filterE (match Key'Escape) keyE
-         reactimate $ foo <$ filterE (match Key'H) keyE
+         reactimate $ sayHello <$ filterE (match Key'H) keyE
          reactimate $ print <$> keyE
          c <- cursor h TopLeft
          reactimate $ putStrLn . ("Cursor: " ++) . show <$> cursorMove c
-     actuate network
+    
+         let i = 1::Int
+         let ecount = accumE 0 ((+i) <$ filterE (match Key'Up) keyE)
+         reactimate $ fmap print ecount
+--     return ecount
+     actuate network -- >> return (1::Int) >>= \x -> print x -- | Maybe that's how I can pass a stateful object to the drawLoop?
+     drawIn Default window ds
+--     return ecount
 
 
-foo :: IO ()
-foo = putStrLn "hello!"
-
-exit :: Window -> IO ()
-exit win = 
-     do
-      shutdown win
-      exitSuccess
+sayHello :: IO ()
+sayHello = print "hello!"
--- tutorial08/README.md
+++ tutorial08/README.md
@@ -2,7 +2,7 @@ Work in Progress :: It's not finieshed yet!
 
 User Interaction
 
-Where we use [reactive-banana](https://github.com/HeinrichApfelmus/reactive-banana) as a FRP library and a brilliant [reactive-banana-glfw](https://github.com/cdxr/reactive-banana-glfw) as a glue for interacting with the program.
+Where we use [reactive-banana](https://github.com/HeinrichApfelmus/reactive-banana) as an FRP library and a brilliant [reactive-banana-glfw](https://github.com/cdxr/reactive-banana-glfw) as a glue, for interacting with the program.
 The scale of the shape will be controlled by events, corresponding to key-presses.
 
 Future work: Next we'll combine it with animation.
