9f64035d263b4d9e6de84903322cd329ec8fec6d
madjestic <madjestic13@gmail.com>
2014-06-03 00:57:19 +0200

    cleaning up a bit

--- tutorial08/Main.hs
+++ tutorial08/Main.hs
@@ -15,10 +15,7 @@ import Reactive.Banana.GLFW
 
 main :: IO ()
 main = do
-     let drawables = [
-                 -- | toDrawable Color $ Shape  Position Radius Divisions
-                      toDrawable White $ Circle (0.0, 0.0) 0.5 3       
-                     ]
+     let drawables = [ toDrawable White $ Circle (0.0, 0.0) 0.5 3 ]
 
      window <- createWindow "NGL is Not GLoss" (512,512)     
      drawIn Default window drawables
@@ -30,25 +27,18 @@ withEventsIn :: Window -> [Drawable] -> IO ()
 withEventsIn window ds = do
      handle <- windowHandler window             
      network <- compile $ do
+     
+         -- | Keyboard events
          keyE <- keyEvent handle
          reactimate $ exit window <$ filterE (match Key'Escape) keyE
          reactimate $ print <$> keyE
+         
+         let ecount = accumE 0 $ ((+1) <$ filterE (match Key'Up) keyE) `union` ((subtract 1) <$ filterE (match Key'Down) keyE)
+         reactimate $ fmap (\x->   ( drawIn Default window [ toDrawable White $ Circle (0.0, 0.0) 1.0 (x+3)] )   ) ecount
+         
+         -- | Mouse events:
          c <- cursor handle TopLeft
          reactimate $ putStrLn . ("Cursor: " ++) . show <$> cursorMove c
-         let ecount = accumE 0 $ ((+1) <$ filterE (match Key'Up) keyE) `union` ((subtract 1) <$ filterE (match Key'Down) keyE)
-         reactimate $ fmap (\x->   ( drawFoo window [ toDrawable White $ Circle (0.0, 0.0) 1.0 (x+3)] )   ) ecount
-
+         
      actuate network
      forever pollEvents
-
-fromEvent :: Event t Int -> Int
-fromEvent = undefined
-
-
-drawFoo :: Window -> [Drawable] -> IO ()
-drawFoo window ds = drawIn Default window ds
-
-
-sayHello :: IO ()
-sayHello = print "hello!"
-
